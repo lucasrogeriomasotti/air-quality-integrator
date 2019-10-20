220c6972aadaddcbaf9d558800d4cb910044b290
Lucas Rogério Masotti <lucas_masotti@sicredi.com.br>
2019-10-20 10:39:29 -0300

    Addin values to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+micrograms_per_cubic_meter = AirQualityParameterUnit.create!(name: 'Micrograms per cubic meter', symbol: 'µg/m3')
+ppm = AirQualityParameterUnit.create!(name: 'Parts per million', symbol: 'ppm')
+
+AirQualityParameter.create(name: 'PM 2.5', description: 'Particulate matter less than 2.5 micrometers in diameter', air_quality_parameter_unit: micrograms_per_cubic_meter)
+AirQualityParameter.create(name: 'PM 10', description: 'Particulate matter less than 10 micrometers in diameter', air_quality_parameter_unit: micrograms_per_cubic_meter)
+AirQualityParameter.create(name: 'Ozone', air_quality_parameter_unit: ppm)
+AirQualityParameter.create(name: 'Sulfur Dioxide SO2 ', air_quality_parameter_unit: ppm)
